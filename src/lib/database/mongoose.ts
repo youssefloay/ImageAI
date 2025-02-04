e34ddf0272fea850d76519a83cdbdf9890ab7690
Youssef Loay <youssefloay@MacBook-Air-M3.local>
2025-02-04 16:35:02 +0100

    models creation

--- src/lib/database/mongoose.ts
+++ src/lib/database/mongoose.ts
@@ -1,28 +1,30 @@
-import mongoose, {Mongoose} from 'mongoose'
+import mongoose, { Mongoose } from "mongoose";
 
-
-const MONGODB_URI = process.env.MONGO_URI
+const MONGODB_URI = process.env.MONGO_URI;
 
 interface MongooseConnection {
-    conn: Mongoose | null;
-    promise: Promise<Mongoose> | null;
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
 }
 
-let cached: MongooseConnection = (global as any).mongoose
+let cached: MongooseConnection = (global as any).mongoose;
 
 if (!cached) {
-    cached = (global as any).mongoose = { conn: null, promise: null }
+  cached = (global as any).mongoose = { conn: null, promise: null };
 }
 
 export const connectToDatabase = async () => {
-    if (cached.conn) {
-        return cached.conn
-    }
-if (MONGODB_URL) throw new Error('Missing MONGODB_URL');
-cached.promise = cached.promise || 
-mongoose.connect(MONGODB_URL,{
-    dbName : 'ImageAI',bufferCommands: false})
+  if (cached.conn) {
+    return cached.conn;
+  }
+  if (MONGODB_URL) throw new Error("Missing MONGODB_URL");
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URL, {
+      dbName: "ImageAI",
+      bufferCommands: false,
+    });
 
-cached.conn = await cached.promise;
-return cached.conn;
-}
+  cached.conn = await cached.promise;
+  return cached.conn;
+};
